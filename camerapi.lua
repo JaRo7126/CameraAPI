5e1c190d8f685a590bcaf856ee004c5dd776ee3c
JaRo7126 <yaroslavltt@gmail.com>
2025-08-13 12:17:49 +0300

    Added RGON support and offset arg to FollowEntity

--- camerapi.lua
+++ camerapi.lua
@@ -16,18 +16,22 @@ CameraAPI.CameraMode = {
 
 local ROOM_DATA = {}
 
+if REPENTOGON then
+	if not CAMERAPI_REPENTOGON_DATA then
+		CAMERAPI_REPENTOGON_DATA = {
+			timeout = -1,
+			mode = 1,
+			follow_offset = Vector.Zero
+		}
+	else
+		CAMERAPI_REPENTOGON_DATA.timeout = -1
+		CAMERAPI_REPENTOGON_DATA.mode = 1
+		CAMERAPI_REPENTOGON_DATA.follow_offset = Vector.Zero
+	end
+end
+
 local function PostCameraUpdate()
-	local camera = CameraAPI:GetCamera()
 	local rooms = Game():GetLevel():GetRooms()
-	camera.State = NpcState.STATE_APPEAR_CUSTOM
-
-	if camera:GetSprite():GetFrame() > 2 then
-		camera:GetSprite():SetFrame(2)
-	end
-
-	if camera.EntityCollisionClass ~= EntityCollisionClass.ENTCOLL_NONE then
-		camera.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
-	end
 
 	if CameraAPI:GetCameraTimeout() > 0 then
 		CameraAPI:SetCameraTimeout(CameraAPI:GetCameraTimeout() - 1)
@@ -53,6 +57,44 @@ local function PostCameraUpdate()
 
 				roomDesc.Flags = roomDesc.Flags | RoomDescriptor.FLAG_NO_WALLS
 			end
+		end
+	end
+
+	if CAMERAPI_REPENTOGON_DATA then
+		if CameraAPI:GetCameraTimeout() ~= 0 then
+			local pos
+
+			if CAMERAPI_REPENTOGON_DATA.follow then
+
+				if type(CAMERAPI_REPENTOGON_DATA.follow) == "Vector" then
+					pos = CAMERAPI_REPENTOGON_DATA.follow
+
+				elseif CAMERAPI_REPENTOGON_DATA.follow.Type then
+					pos = CAMERAPI_REPENTOGON_DATA.follow.Position + CAMERAPI_REPENTOGON_DATA.follow_offset
+				end
+			end
+
+			Game():GetRoom():GetCamera():SetFocusPosition(pos)
+		else
+			if CAMERAPI_REPENTOGON_DATA.follow then
+				CAMERAPI_REPENTOGON_DATA.follow = nil
+			end
+
+			if tostring(CAMERAPI_REPENTOGON_DATA.follow_offset) ~= "0.0 0.0" then
+				CAMERAPI_REPENTOGON_DATA.follow_offset = Vector.Zero
+			end
+		end
+
+	else
+		local camera = CameraAPI:GetCamera()
+		camera.State = NpcState.STATE_APPEAR_CUSTOM
+
+		if camera:GetSprite():GetFrame() > 2 then
+			camera:GetSprite():SetFrame(2)
+		end
+
+		if camera.EntityCollisionClass ~= EntityCollisionClass.ENTCOLL_NONE then
+			camera.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
 		end
 	end
 end
@@ -123,13 +165,21 @@ local function PostCameraRender()
 
 	else
 		if data.follow then
-			local entity = data.follow
+			if type(data.follow) == "Vector" then
+				if tostring(camera.Position) ~= tostring(data.follow) then
+					camera.Position = data.follow
+				end
 
-			if entity:Exists() then
-				camera.Position = entity.Position + entity.Velocity
-			else
-				data.follow = nil
-				CameraAPI:SetCameraLocked(true)
+			elseif data.follow.Type then
+				local entity = data.follow
+
+				if entity:Exists() then
+					camera.Position = entity.Position + entity.Velocity + data.follow_offset
+				else
+					data.follow = nil
+					data.follow_offset = Vector.Zero
+					CameraAPI:SetCameraLocked(true)
+				end
 			end
 		end
 
@@ -139,20 +189,20 @@ local function PostCameraRender()
 			if data.follow then
 				data.follow = nil
 			end
+
+			if tostring(data.follow_offset) ~= "0.0 0.0" then
+				data.follow_offset = Vector.Zero
+			end
 		end
 	end
 end
 
-function CameraAPI:Init(mod)
-	if not mod.CameraAPIInit then
-		CameraAPI.Mod = mod
-
-		mod:AddCallback(ModCallbacks.MC_POST_UPDATE, PostCameraUpdate)
-		mod:AddCallback(ModCallbacks.MC_POST_RENDER, PostCameraRender)
-	end
-end
-
 function CameraAPI:GetCamera()
+	if REPENTOGON then
+		Console.PrintWarning("[" .. CameraAPI.Mod.Name .. "] Warning in CameraAPI_GetCamera: this function will do nothing if REPENTOGON is enabled.")
+		return
+	end
+
 	local camera
 
 	for _, entity in ipairs(Isaac.FindByType(EntityType.ENTITY_HUSH)) do
@@ -173,6 +223,11 @@ function CameraAPI:GetCamera()
 end
 
 function CameraAPI:SpawnCamera()
+	if REPENTOGON then
+		Console.PrintWarning("[" .. CameraAPI.Mod.Name .. "] Warning in CameraAPI_SpawnCamera: this function will do nothing if REPENTOGON is enabled.")
+		return
+	end
+
 	local camera = Isaac.Spawn(EntityType.ENTITY_HUSH, 0, 0,
 							Vector(0, 0), Vector(0, 0), nil):ToNPC()
 	camera.Size = 0
@@ -198,15 +253,24 @@ function CameraAPI:SpawnCamera()
 end
 
 function CameraAPI:IsCameraLocked()
+	if REPENTOGON then
+		Console.PrintWarning("[" .. CameraAPI.Mod.Name .. "] Warning in CameraAPI_IsCameraLocked: this function will do nothing if REPENTOGON is enabled.")
+		return
+	end
+
 	return CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].locked
 end
 
 function CameraAPI:SetCameraLocked(value)
+	if REPENTOGON then
+		Console.PrintWarning("[" .. CameraAPI.Mod.Name .. "] Warning in CameraAPI_SetCameraLocked: this function will do nothing if REPENTOGON is enabled.")
+		return
+	end
+
 	if not value then
 		value = false
 	elseif type(value) ~= "boolean" then
-		error("bad argument #1 to CameraAPI_SetCameraLocked (boolean expected, got " .. type(value) .. ")")
-		Isaac.DebugString(CameraAPI.Mod.Name .. " error in SetCameraLocked(): value is not a boolean (" .. tostring(value or "nil") .. ")")
+		error("[" .. CameraAPI.Mod.Name .. "] bad argument #1 to CameraAPI_SetCameraLocked (boolean expected, got " .. type(value) .. ")")
 		return
 	end
 
@@ -214,48 +278,64 @@ function CameraAPI:SetCameraLocked(value)
 end
 
 function CameraAPI:GetCameraTimeout()
-	return CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].timeout
+	return CAMERAPI_REPENTOGON_DATA and CAMERAPI_REPENTOGON_DATA.timeout
+	or CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].timeout
 end
 
 function CameraAPI:SetCameraTimeout(value)
 	if not value or type(value) ~= "number" then
-		error("bad argument #1 to CameraAPI_SetCameraTimeout (number expected, got " .. type(value) .. ")")
-		Isaac.DebugString(CameraAPI.Mod.Name .. " error in SetCameraTimeout(): value is NAN (" .. tostring(value or "nil") .. ")")
+		error("[" .. CameraAPI.Mod.Name .. "] bad argument #1 to CameraAPI_SetCameraTimeout (number expected, got " .. type(value) .. ")")
 		return
 	end
 
-	CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].timeout = value
+	if CAMERAPI_REPENTOGON_DATA then
+		CAMERAPI_REPENTOGON_DATA.timeout = value
+	else
+		CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].timeout = value
+	end
 end
 
 function CameraAPI:SetCameraPosition(pos, duration, force)
 	if duration == nil then duration = -1 end
 	if force == nil then force = true end
 
-	if force or (not force and not CameraAPI:IsCameraLocked()) then
+	if not CAMERAPI_REPENTOGON_DATA 
+		and (force or (not force and not CameraAPI:IsCameraLocked())) then
 		CameraAPI:GetCamera().Position = pos
+		CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].follow = pos
 		CameraAPI:SetCameraLocked(false)
+	elseif CAMERAPI_REPENTOGON_DATA  then
+		CAMERAPI_REPENTOGON_DATA.follow = pos
+		CAMERAPI_REPENTOGON_DATA.timeout = duration
 	end
 
 	CameraAPI:SetCameraTimeout(duration)
 end
 
-function CameraAPI:CameraFollowEntity(entity, duration, force)
+function CameraAPI:CameraFollowEntity(entity, duration, offset, force)
 	if duration == nil then duration = -1 end
 	if force == nil then force = true end
 
-	if force or (not force and not CameraAPI:IsCameraLocked()) then
+	if not CAMERAPI_REPENTOGON_DATA 
+		and (force or (not force and not CameraAPI:IsCameraLocked())) then
 		local camera = CameraAPI:GetCamera()
 
 		camera.Position = entity.Position
 		camera:GetData()["CameraAPI.CAMERA_DATA"].follow = entity
+		camera:GetData()["CameraAPI.CAMERA_DATA"].follow_offset = offset
 		CameraAPI:SetCameraLocked(false)
+	elseif CAMERAPI_REPENTOGON_DATA  then
+		CAMERAPI_REPENTOGON_DATA.follow = entity
+		CAMERAPI_REPENTOGON_DATA.follow_offset = offset
+		CAMERAPI_REPENTOGON_DATA.timeout = duration
 	end
 
 	CameraAPI:SetCameraTimeout(duration)
 end
 
 function CameraAPI:GetCameraMode()
-	return CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].mode
+	return CAMERAPI_REPENTOGON_DATA and CAMERAPI_REPENTOGON_DATA.mode
+	or CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].mode
 end
 
 function CameraAPI:SetCameraMode(mode)
@@ -264,12 +344,28 @@ function CameraAPI:SetCameraMode(mode)
 		or mode > 2
 		or mode < 1 then
 
-		error("bad argument #1 to CameraAPI_SetCameraMode (CameraMode expected, got " .. type(value) .. ")")
-		Isaac.DebugString(CameraAPI.Mod.Name .. " error in SetCameraMode(): unknown CameraMode (" .. tostring(mode or "nil") .. ")")
+		error("[" .. CameraAPI.Mod.Name .. "] bad argument #1 to CameraAPI_SetCameraMode (CameraMode expected, got " .. type(value) .. ")")
 		return
 	end
 
-	CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].mode = mode
+	if CAMERAPI_REPENTOGON_DATA then
+		CAMERAPI_REPENTOGON_DATA.mode = mode
+	else
+		CameraAPI:GetCamera():GetData()["CameraAPI.CAMERA_DATA"].mode = mode
+	end
+end
+
+
+function CameraAPI:Init(mod)
+	if not mod.CameraAPIInit then
+		CameraAPI.Mod = mod
+
+		mod:AddCallback(ModCallbacks.MC_POST_UPDATE, PostCameraUpdate)
+
+		if not CAMERAPI_REPENTOGON_DATA then
+			mod:AddCallback(ModCallbacks.MC_POST_RENDER, PostCameraRender)
+		end
+	end
 end
 
 
